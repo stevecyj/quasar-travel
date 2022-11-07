5c4113555155d385e5f04fd227699ea8d2e4073d
Steve <steve.tsao@hlw.lan>
2022-11-07 17:54:15 +0800

    feat:secton carousel

---
+++ src/pages/Index/IndexHome.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page>
+
+    <SectionCarousel />
+  </q-page>
+</template>
+
+<script>
+import SectionCarousel from './SectionCarousel.vue'
+export default {
+  components: {
+    SectionCarousel
+  }
+}
+</script>
---
+++ src/pages/Index/SectionCarousel.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="q-pa-md">
+    <q-carousel animated v-model="slide" control-color="grey-1" arrows infinite autoplay height="400px">
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+    </q-carousel>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      slide: ref(1)
+    }
+  }
+}
+</script>
