8e3bd846245ddb246d31eaaadeef098474d45dac
Steve <steve.tsao@hlw.lan>
2022-11-08 14:25:41 +0800

    feat:text autocomplete

--- src/pages/Index/SectionCarousel.vue
+++ src/pages/Index/SectionCarousel.vue
@@ -12,6 +12,20 @@
           <b class="title">一二三四五六七</b>
           <p class="subtitle">仁的本性是</p>
           <p> 這邊明天來做 Input </p>
+
+          <!-- 搜尋框 start-->
+          <q-select bg-color="grey-1" filled :model-value="model" use-input hide-selected fill-input
+            input-debounce="500" :options="options" @filter="advFilter" @input-value="setModel"
+            style="width: 250px; padding-bottom: 32px">
+            <template v-slot:no-option>
+              <q-item>
+                <q-item-section class="text-grey">
+                  No results
+                </q-item-section>
+              </q-item>
+            </template>
+          </q-select>
+          <!-- 搜尋框 end -->
         </q-carousel-control>
       </template>
     </q-carousel>
@@ -20,11 +34,38 @@
 
 <script>
 import { ref } from 'vue'
+import _ from 'lodash'
+
+const locations = [
+  { label: '台北市', icon: 'fas fa-map-marker-alt' },
+  { label: '新北市', icon: 'fas fa-map-marker-alt' },
+  { label: '台中市', icon: 'fas fa-map-marker-alt' },
+  { label: '「網美景點」台中秋紅谷，秋季賞楓勝地', stamp: '台中市' },
+  { label: '東京台場「獨角獸鋼彈」強勢來襲！精彩變身演出搶先看', stamp: '東京, 台場', rightTextColor: 'pink-13' }
+]
 
 export default {
   setup () {
+    const search = ref('')
+    const options = ref(locations)
+
+    const advFilter = (terms, update, abort) => {
+      update(() => {
+
+        const result = _.filter(locations, context => {
+          console.log(terms, locations, context)
+          return context.label.match(terms)
+        })
+        options.value = result
+      })
+
+    }
+
     return {
-      slide: ref(1)
+      slide: ref(1),
+      options,
+      search,
+      advFilter
     }
   }
 }
